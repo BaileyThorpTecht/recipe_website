46c92b1ca71750ccecb0147628d73f9dd52c4c8c
Bailey Thorp <bailey.thorp@gbhs.school.nz>
2024-03-18 09:48:22 +1300

    templates work. now following videos on canvas

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,27 @@
-#from django.shortcuts import render
+import re
+from django.utils.timezone import datetime
 from django.http import HttpResponse
+from django.shortcuts import render
+
+#data that gets passed through to template
+recipes = [
+    {
+        "title": "Soup",
+        "author": "Bailey",
+        "content": "You put the sauce and meat in the bowl then cook it",
+        "date_posted": "today"
+    },
+    {
+        "title": "beans",
+        "author": "Billy",
+        "content": "You plant em and you grow em",
+        "date_posted": "January 8th 2023"
+    }
+]
+
 
 def home(request):
-    return HttpResponse("Hello, Django")
+    context = {
+        'recipes': recipes
+    }
+    return render(request, 'hello/home.html', context)
