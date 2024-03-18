351f597fdcc10cf6285d86e767c84631ce0a4b89
Bailey Thorp <bailey.thorp@gbhs.school.nz>
2024-03-19 12:21:02 +1300

    in the middle of creating user registration system. video 29 complete

--- hello/views.py
+++ hello/views.py
@@ -1,27 +1,13 @@
-import re
-from django.utils.timezone import datetime
-from django.http import HttpResponse
 from django.shortcuts import render
-
-#data that gets passed through to template
-recipes = [
-    {
-        "title": "Soup",
-        "author": "Bailey",
-        "content": "You put the sauce and meat in the bowl then cook it",
-        "date_posted": "today"
-    },
-    {
-        "title": "beans",
-        "author": "Billy",
-        "content": "You plant em and you grow em",
-        "date_posted": "January 8th 2023"
-    }
-]
-
+from django.http import HttpResponse
+from . import models
 
 def home(request):
+    recipes = models.Recipe.objects.all()
     context = {
         'recipes': recipes
     }
     return render(request, 'hello/home.html', context)
+
+def about(request):
+    return render(request, 'hello/home.html', {'title': 'about page'})
